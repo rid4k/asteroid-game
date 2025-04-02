10f2bd77c40cf89f5f840cbf1742dfd9814591ab
kadirkattas <kattaskadir0@gmail.com>
2025-04-03 00:51:31 +0300

    circle shape class and it's child player class created, also implemented

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 
 
@@ -8,13 +9,22 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+    dt = 0
+    time_clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (1,1,1))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = time_clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
