e1cf7bdbb406d043b04a46d207c0bc1171c44250
kadirkattas <kattaskadir0@gmail.com>
2025-04-12 12:40:25 +0300

    kill and split features are added

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
 from circleshape import *
+from constants import *
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -9,3 +11,40 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        angle = random.uniform(20, 50)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        new1_velocity = self.velocity.rotate(angle)
+        new2_velocity = self.velocity.rotate(-angle)
+        
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = new1_velocity * 1.2
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = new2_velocity * 1.2
+        
+        
+
+        # self.kill()
+
+        # if self.radius <= ASTEROID_MIN_RADIUS:
+        #     return
+
+        # # randomize the angle of the split
+        # random_angle = random.uniform(20, 50)
+
+        # a = self.velocity.rotate(random_angle)
+        # b = self.velocity.rotate(-random_angle)
+
+        # new_radius = self.radius - ASTEROID_MIN_RADIUS
+        # asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        # asteroid.velocity = a * 1.2
+        # asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        # asteroid.velocity = b * 1.2
--- main.py
+++ main.py
@@ -39,6 +39,11 @@ def main():
             if asteroid.check_collision(player):
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                    if asteroid.check_collision(shot):
+                        shot.kill()
+                        asteroid.split()
+
         for sprite in drawable:
             sprite.draw(screen)
         pygame.display.flip()
